02a3f9ff9188f8577ecb96bb3067c40e691de48e
arctelix <arctelix@gmail.com>
2016-05-14 13:23:28 -0400

    dotsys update \b \b

---
+++ test_from_alt/topic.sh
@@ -0,0 +1,19 @@
+install () {
+    echo "topic.sh install ran successfully"
+}
+
+uninstall () {
+    echo "topic.sh uninstall ran successfully"
+}
+
+upgrade () {
+    echo "topic.sh upgrade ran successfully"
+}
+
+update () {
+    echo "topic.sh update ran successfully"
+}
+
+freeze () {
+    echo "topic.sh freeze ran successfully"
+}
