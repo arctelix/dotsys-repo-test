a60a59ce5618bb8275c38cdc70f26b1b8d502091
arctelix <arctelix@gmail.com>
2016-05-14 01:29:00 -0400

    dotsys uninstall  test_alt_symlink test_bin test_cfg_exclude_mac test_dep1 test_dep2 test_has_deps test_ismanaged test_manager test_n_ismanaged test_scripts test_settings test_source_sh test_t_script

--- test_manager/manager.sh
+++ test_manager/manager.sh
@@ -1,27 +1,21 @@
 install () {
 printf "manager.sh install ran successfully"
-printf "\nparams: $@"
-
 }
 
 uninstall () {
 printf "manager.sh uninstall ran successfully"
-printf "\nparams: $@"
 }
 
 upgrade () {
 printf "manager.sh upgrade ran successfully"
-printf "\nparams: $@"
 }
 
 update () {
 printf "manager.sh update ran successfully"
-printf "\nparams: $@"
 }
 
 freeze () {
 printf "manager.sh freeze ran successfully"
-printf "\nparams: $@"
 }
 
 $@
