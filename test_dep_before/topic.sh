7268a616f81c0a7aa38878dd3b9b8dda96089d0c
arctelix <arctelix@gmail.com>
2016-05-15 13:00:21 -0400

    dotsys install test_has_deps

---
+++ test_dep_before/topic.sh
@@ -0,0 +1,26 @@
+install () {
+printf "topic.sh install ran successfully"
+printf "\nparams: $@"
+}
+
+uninstall () {
+printf "topic.sh uninstall ran successfully"
+printf "\nparams: $@"
+}
+
+upgrade () {
+printf "topic.sh upgrade ran successfully"
+printf "\nparams: $@"
+}
+
+update () {
+printf "topic.sh update ran successfully"
+printf "\nparams: $@"
+}
+
+freeze () {
+printf "topic.sh freeze ran successfully"
+printf "\nparams: $@"
+}
+
+$@
